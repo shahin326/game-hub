46a4ab38713986e76936628072815b1c48275265
shahinsa <shahin.safari@optiscangroup.com>
2023-09-12 16:52:07 +0300

    added filter

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,33 @@ import DismissableAlert from "./components/DismissableAlert";
 import ExpandableText from "./components/ExpandableText";
 import Form from "./expense-tracker/components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(""); // we need to this to cause rerender
   const [expenses, setExpenses] = useState([
     { id: 1, amount: 10, description: "blah", category: "Utility" },
     { id: 2, amount: 4, description: "rff", category: "Food" },
     { id: 3, amount: 50, description: "ccx", category: "Drink" },
   ]);
 
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expense) => expense.category === selectedCategory)
+    : expenses;
+
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        ></ExpenseFilter>
+      </div>
+
       <ExpenseList
-        expenses={expenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+        expenses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(visibleExpenses.filter((e) => e.id !== id))
+        }
       ></ExpenseList>
     </div>
   );
---
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface Props {
+  onSelectCategory: (category: string) => void;
+}
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
+  return (
+    <select
+      className="form-select"
+      onChange={(event) => onSelectCategory(event.target.value)}
+    >
+      <option value="">All categories</option>
+      <option value="Food">Food</option>
+      <option value="Drink">Drink</option>
+      <option value="Utility">Utility</option>
+    </select>
+  );
+};
+
+export default ExpenseFilter;
